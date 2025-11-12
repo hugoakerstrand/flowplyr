5c307250dd97b66fe99f594342bba0d8ff30c5fa
hugoakerstrand <hugo.akerstrand@gmail.com>
2025-11-12 21:52:58 +0000

    Add function to_tibble: intended to convert flowSet to a tibble format using keywords to access specific slots. Name is more open ended, which is intentional since other S4 objects might be included for the function at a later stage.

---
+++ R/to_tibble.R
@@ -0,0 +1,49 @@
+to_tibble <- function(data, ...) {
+  input_keywords <- c(...)
+
+  # Input validation
+  if(!any(class(data) == "flowSet")) {
+    cli::cli_abort("{.obj data} must have class {.cls flowSet}, not {.cls {class(data)[1]}}.")
+  }
+
+  valid_keywords <- c("all", "sample_names", "exprs", "keywords", "meta_data")
+
+ if(!missing(...) && !any(input_keywords %in% valid_keywords)) {
+    cli::cli_abort("`...` must be a combination of {.val {valid_keywords}}, not {.val {(input_keywords)}}.")
+ }
+
+  # Handle default "all" case to access complete data
+  if (missing(...) || "all" %in% input_keywords) {
+    input_keywords <- setdiff(valid_keywords, "all")
+  }
+
+  # Collect data using input_keywords
+  result_list <- list()
+
+  for (kw in input_keywords) {
+    result_list[[kw]] <- switch(kw,
+                                sample_names = get_sample_names(data),
+                                exprs = get_exprs(data),
+                                keywords = get_keywords(data),
+                                meta_data = get_meta_data(data)
+                        )
+  }
+
+  do.call(tibble::tibble, result_list)
+}
+
+get_sample_names <- function(data) {
+  flowCore::pData(data) |> rownames()
+}
+
+get_exprs <- function(data) {
+  lapply(1:length(data), function(i) flowCore::exprs(data[[i]]))
+}
+
+get_keywords <- function(data) {
+  lapply(1:length(data), function(i) flowCore::keyword(data[[i]]))
+}
+
+get_meta_data <- function(data) {
+  flowCore::pData(data)
+}
